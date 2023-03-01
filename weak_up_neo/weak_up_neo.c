9e5a089563d392f15273af8b624977dc3af3df65
Victor Karpov <nilafe4@gmail.com>
2023-03-01 15:46:42 +0500

    fix

--- weak_up_neo/weak_up_neo.c
+++ weak_up_neo/weak_up_neo.c
@@ -4,6 +4,6 @@ int main() {
   // put your code here
   printf("Wake up, Neo...\n\n");
   printf("The Matrix has you...\n\n");
-  printf("Follow the white rabbit...");
+  printf("Follow the white rabbit...\n");
   return 0;
 }
