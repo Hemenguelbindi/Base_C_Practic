749586b5063af982b39872047a7c6d3203255647
Victor Karpov <nilafe4@gmail.com>
2023-03-01 15:34:38 +0500

    weak up neo

---
+++ weak_up_neo/weak_up_neo.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  // put your code here
+  printf("Wake up, Neo...\n\n");
+  printf("The Matrix has you...\n\n");
+  printf("Follow the white rabbit...");
+  return 0;
+}
